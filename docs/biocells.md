555800a4ec65e8ea2ca8549a60cefbee31050346
stephan192 <stephan192@outlook.com>
2024-03-15 21:12:30 +0100

    ADD: DwdBioWeatherAPI

---
+++ docs/biocells.md
@@ -0,0 +1,13 @@
+| Cell ID | Name |
+|---------|------|
+| 1 | Schles.-Holst., Hamburg, Bremen, nörd. Niedersach. |
+| 2 | Mecklenburg-Vorpommern |
+| 3 | Südwestliches Niedersachsen, Nordrhein-Westfalen |
+| 4 | Östliches und südliches Niedersachsen |
+| 5 | Berlin, Brandenburg, nördliches Sachsen-Anhalt |
+| 6 | Südliches Sachsen-Anhalt, Thüringen, Sachsen |
+| 7 | Hessen, Rheinland-Pfalz, Saarland |
+| 8 | Baden |
+| 9 | Württemberg, Franken ohne östliches Oberfranken |
+| 10 | Niederbayern, Oberpfalz, östliches Oberfranken |
+| 11 | Schwaben, Oberbayern |
---
+++ tests/test_bioweather.py
@@ -0,0 +1,75 @@
+"""Tests for dwdwfsapi bioweather module."""
+
+from datetime import UTC, datetime, timedelta
+
+import pytest
+
+from dwdwfsapi import DwdBioWeatherAPI
+
+MIN_LEVEL = 0  # 0 = positive impact
+MAX_LEVEL = 3  # 3 = high risk
+TIME_TOLERANCE = 5  # seconds
+FORECAST_TYPES = 6  # Number of different forecast types
+FORECAST_COUNT = 6  # Number forecasts per type
+
+testdata_ident = [
+    (2, "Mecklenburg-Vorpommern"),
+    (7, "Hessen, Rheinland-Pfalz, Saarland"),
+]
+
+testdata_name = [
+    ("Östliches und südliches Niedersachsen", 4),
+    ("Schwaben, Oberbayern", 11),
+]
+
+testdata_invalid = [12, "Hintertupfing"]
+
+
+@pytest.mark.parametrize("ident, name", testdata_ident)
+def test_id(ident, name):
+    """Test a given cell id."""
+    dwd = DwdBioWeatherAPI(ident)
+
+    assert dwd.data_valid
+    assert dwd.cell_id == ident
+    assert dwd.cell_name == name
+    start_time = datetime.now(UTC) - timedelta(0, TIME_TOLERANCE)
+    stop_time = start_time + timedelta(0, (2 * TIME_TOLERANCE))
+    assert start_time < dwd.last_update < stop_time
+    assert isinstance(dwd.forecast_data, dict)
+    assert len(dwd.forecast_data) == FORECAST_TYPES
+    for v in dwd.forecast_data.values():
+        assert len(v["forecast"]) == FORECAST_COUNT
+        for entry in v["forecast"]:
+            assert MIN_LEVEL <= entry["level"] <= MAX_LEVEL
+
+
+@pytest.mark.parametrize("name, ident", testdata_name)
+def test_name(name, ident):
+    """Test a given cell name."""
+    dwd = DwdBioWeatherAPI(ident)
+
+    assert dwd.data_valid
+    assert dwd.cell_id == ident
+    assert dwd.cell_name == name
+    start_time = datetime.now(UTC) - timedelta(0, TIME_TOLERANCE)
+    stop_time = start_time + timedelta(0, (2 * TIME_TOLERANCE))
+    assert start_time < dwd.last_update < stop_time
+    assert isinstance(dwd.forecast_data, dict)
+    assert len(dwd.forecast_data) == FORECAST_TYPES
+    for v in dwd.forecast_data.values():
+        assert len(v["forecast"]) == FORECAST_COUNT
+        for entry in v["forecast"]:
+            assert MIN_LEVEL <= entry["level"] <= MAX_LEVEL
+
+
+@pytest.mark.parametrize("ident_name", testdata_invalid)
+def test_wrong_input(ident_name):
+    """Test an invalid input."""
+    dwd = DwdBioWeatherAPI(ident_name)
+
+    assert not dwd.data_valid
+    assert dwd.cell_id is None
+    assert dwd.cell_name is None
+    assert dwd.last_update is None
+    assert dwd.forecast_data is None
